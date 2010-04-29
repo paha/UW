1e8f00173431a3d29e7190a200006f74499960e4
paha <paha@chuda.local>
2010-04-29 08:32:49 -0700

    layout changes at week5

--- taskmaster/lib/auth/client.rb
+++ taskmaster/lib/auth/client.rb
@@ -2,23 +2,77 @@
 
 ###
 # Student Name: Pavel Snagovsky 
-# Homework Week: 3
+# Homework Week: 5
 #
 ###
 # Description:
-#
-# Creating DSL
 # Authentication client
 # 
 
-require File.dirname(__FILE__) + '/auth_client_class'
+require 'socket'
+require 'digest/md5'
 
-unless ARGV.length == 2
-  puts "Expecting username and password."
-  exit 1
-end 
+# Authentication client class
+class AuthClient
+  
+  HOST = 'localhost'
+  
+  def initialize( user, passwd, debug = true, port = 24842 )
+    @debug = debug
+    begin
+      @con = TCPSocket.new( HOST, port )
+    rescue
+      debug_msg "Failed to connect to #{HOST}:#{port}"
+      exit 1
+    end
+    debug_msg "--- Successfully connected to auth server. #{Time.now}"  
+    authenticate( user, passwd )  
+  end
+  
+  # output debug messages to stdout, unless debug isn't 'true'. Wasn't sure if messages should go to a file or not.
+  def debug_msg( str )
+    puts str if @debug == true
+  end
+  
+  def get_salt( user )
+    @con.puts user
+    return @con.gets.chomp
+  end
+  
+  def verify_salt?( salt )
+    # Auth server will return NOT AUTHORIZED, if it wasn't able to verify the user
+    if salt == "NOT AUTHORIZED" or salt.length != 32
+      debug_msg salt
+      return false
+    end
+  end
+  
+  def send_salty_passwd( passwd, salt )
+    passwd_hash = Digest::MD5.hexdigest( salt + passwd )
+    @con.puts passwd_hash
+    return @con.gets.chomp
+  end
+  
+  def authenticate( user, passwd )
+    debug_msg "--- Sending username \"#{user}\""
+    salt = get_salt( user )
+    exit 1 if verify_salt?( salt ) == false
+    debug_msg "--- Recieved \"salt\" from the server. Cooking..."
+    debug_msg "--- Sending encrypted passwd"
+    result = send_salty_passwd( passwd, salt )
+    debug_msg "#{result}"
+  end
+  
+end
 
-user = ARGV[0]
-passwd = ARGV[1]
+if __FILE__ == $0
+  unless ARGV.length == 2
+    puts "Expecting username and password."
+    exit 1
+  end 
 
-AuthClient.new( user, passwd )
+  user = ARGV[0]
+  passwd = ARGV[1]
+
+  AuthClient.new( user, passwd )
+end
--- taskmaster/lib/auth/server.rb
+++ taskmaster/lib/auth/server.rb
@@ -6,11 +6,92 @@
 #
 ###
 # Description:
-#
-# Creating DSL
 # Authentication server
 # 
 
-require File.dirname(__FILE__) + '/auth_server_class'
+require 'yaml'
+require 'socket'
+require 'digest/md5'
 
-AuthServer.new()
+# Authentication server class
+class AuthServer
+  # Authentication sequence executed initializing the class
+  def initialize( port = 24842, debug = true )
+    @debug = debug
+    data = YAML.load_file( Dir.pwd + '/passwd' )    
+    auth_server = TCPServer.new( port )
+    auth_sequence( port, data, auth_server )
+  end
+  
+  # Authentication sequence with debug messages, error mesgs will come from corresponding methods
+  def auth_sequence( port, data, auth_server )
+    while true
+      debug_msg "\nReady to accept new connections. Listening on port: #{port}. #{Time.now}"
+      session = auth_server.accept
+      debug_msg "--- #{session.peeraddr[2]} connected. #{Time.now}"
+      user = get_username( session, data )
+      next if user == false
+      debug_msg "--- Received \"#{user}\" for username. #{Time.now}"
+      salt = send_salt( session, user )
+      debug_msg "--- Sent salt to the client. #{Time.now}"
+      passwd_salty = get_salty_passwd( session )
+      debug_msg "--- Received salty password from the client. #{Time.now}"
+      authenticate?( session, salt, passwd_salty, user, data )
+    end
+  end
+  
+  # output debug messages to stdout, unless debug isn't 'true'. Wasn't sure if other loging method was expected.
+  def debug_msg( str )
+    puts str if @debug == true
+  end
+  
+  # Getting username, and verifying it against our data
+  def get_username( session, data )
+    username = session.gets.chomp
+    unless data[username]
+      debug_msg "NOT AUTHORIZED. Unknown username \"#{username}\". Closing session with #{session.peeraddr[2]} #{Time.now}"
+      session.puts "NOT AUTHORIZED"
+      session.close
+      return false
+    end
+    return username
+  end
+  
+  # Generating salt and sending it to the client
+  def send_salt( session, user )
+    salt = Digest::MD5.hexdigest( user + Time.now.strftime('%M%S') + rand(300).to_s )
+    session.puts salt
+    return salt
+  end
+  
+  # Receiving and verifying salt
+  def get_salty_passwd ( session )
+    passwd_salty = session.gets.chomp
+    if passwd_salty.length != 32
+      session.puts "NOT AUTHORIZED"
+      session.close
+      debug_msg "NOT AUTHORIZED. Unexpected salty passwd hash. #{Time.now}"
+      return false
+    end
+    return passwd_salty
+  end
+  
+  # Authentication
+  def authenticate?( session, salt, passwd_salty, user, data )
+    if passwd_salty == Digest::MD5.hexdigest( salt + data[user] )
+      result = "AUTHORIZED"
+      debug_msg "AUTHORIZED \"#{user}\". Closing session. #{Time.now}"
+    else
+      result "NOT AUTHORIZED"
+      debug_msg "NOT AUTHORIZED \"#{user}\". password mismatch. Closing session. #{Time.now}"
+    end
+    session.puts result
+    session.close
+    return result
+  end
+
+end
+
+if __FILE__ == $0
+  AuthServer.new()
+end
